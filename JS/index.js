0136bfa73d1ef69effc452131bd4967c4c893fc8
JeanHFerreira <jean.h.ferreira.xd@gmail.com>
2017-08-10 21:46:22 -0300

    JS\index.js

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,38 @@
-var myTimeTracking = angular.module('myTimeTracking',[]);
+var myTimeTracking = angular.module('myTimeTracking', []);
 
 myTimeTracking.controller('MainCtr', ['$scope', function($scope) {
-  $scope.hello = 'Hola!';
+
+  function init() {
+    $scope.headers = {
+
+    };
+
+    $scope.cadastro = {
+      intensRegistrados: [],
+      itensPendentes: []
+    };
+  }
+
+  function addItem() {
+    $scope.cadastro.itensPendentes.push({
+      trf_codigo: "",
+      prot_codig: "",
+      anot_descricao: "",
+      tptrf_codigo: "",
+      anot_tempo: ""
+    });
+  }
+
+  function removeItem(item) {
+
+  }
+
+  function editTime(item) {
+
+  }
+
+  $scope.addItem = addItem;
+
+
+  init();
 }]);
