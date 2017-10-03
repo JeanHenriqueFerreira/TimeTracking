dbc8a762db854925748db150efac98aa98647526
JeanHFerreira <jean.h.ferreira.xd@gmail.com>
2017-10-03 20:28:52 -0300

    classes

--- public/JS/index.js
+++ public/JS/index.js
@@ -93,7 +93,7 @@ myTimeTracking.controller('MainCtr', ['$scope', '$interval', '$http', '$cookies'
       $scope.indexOrigem = indexItem;
       ngDialog.open({
         templateUrl: './public/Templates/dialogTrocaTempo.html',
-        //className: 'ngdialog-theme-default',
+        className: 'ngdialog-custom',
         scope: $scope
       });
     }
